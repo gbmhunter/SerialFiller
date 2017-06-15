93723578acb65b31611fc6a5e7afbe1195a2ac6a
Geoffrey Hunter <ghunter@urthecast.com>
2017-06-15 10:22:29 -0700

    Added multi-topic and data with zeros tests.

--- test/LoopBackTests.cpp
+++ test/LoopBackTests.cpp
@@ -25,7 +25,6 @@ namespace {
     };
 
     TEST_F(LoopBackTests, SingleTopicTest) {
-        auto packet = std::string("test-topic:hello");
         auto topic = std::string("");
         auto data = std::string("");
 
@@ -39,6 +38,50 @@ namespace {
         serialFiller.Publish("test-topic", "hello");
 
         EXPECT_EQ("hello", savedData);
+    }
+
+    TEST_F(LoopBackTests, DataWithZerosTest) {
+        auto topic = std::string("");
+        auto data = std::string("");
+
+        // Subscribe to a test topic
+        std::string savedData;
+        serialFiller.Subscribe("test-topic", [&](std::string data) -> void {
+            savedData = data;
+        });
+
+        // Publish data on topic
+        serialFiller.Publish("test-topic", std::string({ '\x00', '\x00' }));
+
+        EXPECT_EQ(std::string({ '\x00', '\x00' }), savedData);
+    }
+
+    TEST_F(LoopBackTests, MultiTopicTest) {
+        auto topic = std::string("");
+        auto data = std::string("");
+
+        // Subscribe to some topics (sharing the data object)
+        std::string savedData;
+        serialFiller.Subscribe("topic1", [&](std::string data) -> void {
+            savedData = data;
+        });
+        serialFiller.Subscribe("topic2", [&](std::string data) -> void {
+            savedData = data;
+        });
+
+        // Publish data on topic
+        serialFiller.Publish("topic1", "hello");
+        EXPECT_EQ("hello", savedData);
+        savedData.clear();
+
+        serialFiller.Publish("topic2", "world");
+        EXPECT_EQ("world", savedData);
+        savedData.clear();
+
+        // Publish on topic we haven't registered on
+        serialFiller.Publish("topic3", "nope");
+        EXPECT_EQ("", savedData);
+        savedData.clear();
     }
 
 }  // namespace
